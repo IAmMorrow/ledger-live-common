d0390e6fd81e0e6b5adad54e1e6dca5057734ce2
Thomas Wilgenbus <thomas.wilgenbus-ext@ledger.fr>
2020-12-17 13:38:54 +0100

    Values are ranged from [0, 1]

--- src/families/bitcoin/privacyDog/heuristics/outputValue1000x.js
+++ src/families/bitcoin/privacyDog/heuristics/outputValue1000x.js
@@ -18,25 +18,25 @@ export const outputValue1000x: HeuristicHandler = (account: Account) => {
         out2.value.div(out1.value) > 1000
       ) {
         report.operations.push(op);
-        report.penalty += 4;
+        report.penalty += 1;
       } else if (
         out1.value.div(out2.value) > 500 ||
         out2.value.div(out1.value) > 500
       ) {
         report.operations.push(op);
-        report.penalty += 3;
+        report.penalty += 0.8;
       } else if (
         out1.value.div(out2.value) > 100 ||
         out2.value.div(out1.value) > 100
       ) {
         report.operations.push(op);
-        report.penalty += 2;
+        report.penalty += 0.6;
       } else if (
         out1.value.div(out2.value) > 25 ||
         out2.value.div(out1.value) > 25
       ) {
         report.operations.push(op);
-        report.penalty += 1;
+        report.penalty += 0.3;
       }
       return report;
     },
